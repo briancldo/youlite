8a0e85b0ff09b8efc31e5d1c027c170da7174374
Brian Do <briancldo@gmail.com>
2021-02-24 22:27:57 -0500

    styles the video page more neatly

--- ui/src/pages/Video/index.js
+++ ui/src/pages/Video/index.js
@@ -1,17 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
+import Accordion from '@material-ui/core/Accordion';
+import AccordionSummary from '@material-ui/core/AccordionSummary';
+import AccordionDetails from '@material-ui/core/AccordionDetails';
+import ExpandIcon from '@material-ui/icons/ExpandMoreRounded';
 
 import { getQueryObject } from '../../utils/navigation';
 import './index.css';
 
 function VideoUI(props) {
   const { id, title, description, uploader } = props;
+  const [descriptionExpanded, setDescriptionExpanded] = useState(false);
+
+  function toggleDescription() {
+    setDescriptionExpanded(expanded => !expanded);
+  }
 
   return (
     <div className='video-page-main'>
       <h1>{title}</h1>
       <iframe
-        width='560'
+        width={`${Math.min(560, window.innerWidth)}`}
         height='315'
         src={`https://www.youtube.com/embed/${id}`}
         title={title}
@@ -20,7 +29,18 @@ function VideoUI(props) {
         allowFullScreen="allowFullScreen">
       </iframe>
       <p>{uploader}</p>
-      <p>{description}</p>
+      <Accordion
+        expanded={descriptionExpanded}
+        onChange={toggleDescription}
+        className='video-description-accordion'
+      >
+        <AccordionSummary expandIcon={<ExpandIcon />}>
+          <p>Description</p>
+        </AccordionSummary>
+        <AccordionDetails>
+          <p>{description}</p>
+        </AccordionDetails>
+      </Accordion>
     </div>
   )
 }
