7a5e109e550a165362f0b9cc69e92fb3866138e1
Brian Do <briancldo@gmail.com>
2021-02-24 20:48:10 -0500

    moves logout button to settings page

--- ui/src/pages/Home/index.js
+++ ui/src/pages/Home/index.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import Button from '@material-ui/core/Button';
 
 import './index.css';
 
 import { useInitializeState } from '../../utils/initializeData';
 import { getPlaylistsData } from '../../utils/youtube.api';
-import { useRedirect, routes } from '../../utils/navigation';
 import { get, set } from '../../data/playlist';
 import PlaylistList from '../../components/common/PlaylistList';
 
@@ -13,13 +11,11 @@ export default function HomePage() {
   const playlistsCached = Object.keys(get.playlists()).length > 0;
   const getPlaylists = playlistsCached ? get.playlistsDataList : getPlaylistsData;
   const [playlistsData] = useInitializeState(getPlaylists, [], []);
-  const navigateToLogin = useRedirect(routes.login);
 
   if (!playlistsCached && playlistsData.length > 0) set.playlistsData(playlistsData);
 
   return (
     <div className='homepage-main'>
-      <Button onClick={navigateToLogin}>Go to Login</Button>
       <h1>Playlists</h1>
       <PlaylistList {...{playlistsData}} />
     </div>
--- ui/src/pages/Settings/index.js
+++ ui/src/pages/Settings/index.js
@@ -1,5 +1,13 @@
 import React from 'react';
+import Button from '@material-ui/core/Button';
+
+import { removeToken } from '../../data/token';
 
 export default function SettingsPage() {
-  return <h1>Coming soon!</h1>;
+  return (
+  <div>
+    <h1>Settings</h1>
+    <Button onClick={removeToken}>Logout</Button>
+  </div>
+  )
 }
