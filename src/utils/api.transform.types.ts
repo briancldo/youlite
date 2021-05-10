7004712448cc002b26032c5db000e2d428b19a73
Brian Do <briancldo@gmail.com>
2021-05-09 22:52:26 -0400

    fixes ts errors in api.transform util module

---
+++ src/utils/api.transform.types.ts
@@ -0,0 +1,61 @@
+export interface ChannelData {
+  channelTitle: string;
+  thumbnails: {
+    medium: { url: string }
+  };
+}
+
+export interface VideoData {
+  channelData: string;
+  title: string;
+  description: string;
+  thumbnails: {
+    medium: { url: string }
+  };
+}
+
+interface ChanenelSearchResult {
+  id: { kind: 'youtube#channel' };
+  snippet: ChannelData;
+}
+
+interface VideoSearchResult {
+  id: { kind: 'youtube#video' };
+  snippet: VideoData;
+}
+
+export type SearchResult = ChanenelSearchResult | VideoSearchResult;
+
+export interface Playlist {
+  id: string;
+  title: string;
+  description: string;
+  thumbnailUrl: string;
+}
+
+interface PlaylistResult {
+  id: string;
+  snippet: {
+    title: string,
+    description: string,
+    thumbnails: { medium: { url: string } },
+  };
+}
+
+export interface PlaylistResults {
+  items: PlaylistResult[];
+}
+
+interface PlaylistVideosResult {
+  snippet: {
+    resourceId: { videoId: string },
+    title: string,
+    thumbnails: { medium: { url: string } },
+    videoOwnerChannelTitle: string;
+    description: string;
+  };
+}
+
+export interface PlaylistVideosResults {
+  items: PlaylistVideosResult[];
+}
