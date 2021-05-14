d67912a7e72588c5e3a849d1283728e0fcb502fd
Brian Do <briancldo@gmail.com>
2021-05-13 21:51:19 -0400

    fixes ts errors in InteractiveCard component

--- src/components/common/InteractiveCard.tsx
+++ src/components/common/InteractiveCard.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
-import Card from '@material-ui/core/Card';
+import Card, { CardProps } from '@material-ui/core/Card';
 import './InteractiveCard.css';
 
-export default function InteractiveCard(props) {
+const InteractiveCard: React.FC<CardProps> = (props) => {
   return (
     <div className='interactive-card'>
       <Card {...props} />
     </div>
   );
 }
+
+export default InteractiveCard;
