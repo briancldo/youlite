da1d2a6e1bd6a3f178e6b8260290beffcb4a57c7
Brian Do <briancldo@gmail.com>
2021-05-13 21:26:14 -0400

    fixes ts errors in SearchPage component

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -4,13 +4,13 @@ import axios from 'axios';
 import { RequestMethod } from './request.types';
 import config from 'env';
 
-function useFetch(method: RequestMethod, url: string, body = {}, defaultValue: any) {
+function useFetch(method: RequestMethod, url: string, body?: Record<string, any>, defaultValue?: any) {
   const _url = url[0] === '/' ? `${config.get('domain')}${url}}`: url;
   const [data, setData] = useState(defaultValue);
 
   useEffect(() => {
     const fetch = async () => {
-      const response = await axios[method](_url, body);
+      const response = await axios[method](_url, body || {});
       setData(response.data);
     }
     fetch();
